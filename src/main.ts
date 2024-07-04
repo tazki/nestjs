525a7a8ff2875bf799061d594fc58e88b9c6b5a6
Mark Bautista <42196376+tazki@users.noreply.github.com>
2024-07-04 17:33:47 +0800

    [Add] Global MyLoggerModule

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// import { MyLoggerModule } from './my-logger/my-logger.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // const app = await NestFactory.create(AppModule, {
+  //   bufferLogs: true,
+  // });
+  // app.useLogger(app.get(MyLoggerModule));
   app.enableCors();
   app.setGlobalPrefix('api');
   await app.listen(3001);
