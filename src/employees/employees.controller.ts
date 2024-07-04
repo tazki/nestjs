868df4dd818575ef7406ac07a8c774cfe68f3a4e
Mark Bautista <42196376+tazki@users.noreply.github.com>
2024-07-04 18:07:58 +0800

    [Add] Implement Custom Logger

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -7,15 +7,18 @@ import {
   Param,
   Delete,
   Query,
+  Ip,
 } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
 import { Prisma } from '@prisma/client';
 import { Throttle, SkipThrottle } from '@nestjs/throttler';
+import { MyLoggerService } from 'src/my-logger/my-logger.service';
 
 @SkipThrottle()
 @Controller('employees')
 export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) {}
+  private readonly logger = new MyLoggerService(EmployeesController.name);
 
   @Post()
   create(@Body() createEmployeeDto: Prisma.EmployeeCreateInput) {
@@ -24,7 +27,14 @@ export class EmployeesController {
 
   @SkipThrottle({ default: false }) // This will only route that limiter will apply
   @Get()
-  findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+  findAll(
+    @Ip() ip: string,
+    @Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN',
+  ) {
+    this.logger.log(
+      `Request for All Employees\t${ip}`,
+      EmployeesController.name,
+    );
     return this.employeesService.findAll(role);
   }
 
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, HttpAdapterHost } from '@nestjs/core';
 import { AppModule } from './app.module';
-// import { MyLoggerModule } from './my-logger/my-logger.module';
+import { AllExceptionsFilter } from './all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // const app = await NestFactory.create(AppModule, {
-  //   bufferLogs: true,
-  // });
-  // app.useLogger(app.get(MyLoggerModule));
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
+
   app.enableCors();
   app.setGlobalPrefix('api');
   await app.listen(3001);
